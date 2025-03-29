2a9f1a4d9bf16c4a0c606a6e83d1f1ef7d1ea1b5
XanderPENG <pzh980523@outlook.com>
2025-03-29 16:59:44 +0100

    run SA stats

--- src/main/python/freight_emission/run_scenario_analysis.py
+++ src/main/python/freight_emission/run_scenario_analysis.py
@@ -3,7 +3,11 @@ import freight_emissions_anls as fea
 
 logging.basicConfig(level=logging.INFO)
 
-scenario_kw_list = ['basic','van', 'cb']
+scenario_kw_list = [
+    # 'basic',
+    # 'van', 
+    # 'cb'
+    ]
 # The SA-scenario names must be the same as the folder names.
 sa_scenario_kw_list = ['VanSA2t', 'CBSA80kg', 'CBSA100kg', 'CBSA150kg', 'CBSA200kg']  
 iter_idx_list = list(range(300, 400))
